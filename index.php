3d6a70d177a6c0ba28cb53ba6fd6554415adf3ac
Matheus Fernandes <matheusfernandesm2411@gmail.com>
2026-05-12 21:39:51 -0300

    up

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/app.css">
+    <title>Camisas MP</title>
+</head>
+
+<body>
+
+    <div class="container-page">
+
+        <div class="image-side"><img src="img/fundo_login.jpeg" class="img-fluid img-bg fs-1"></div>  
+
+        <div class="form-side">
+
+            <div class="form-card">
+
+                <h1 class="title text-center">Camisas MP</h1>
+
+                <form>
+
+                    <div class="mb-3">
+                        <label class="form-label">Nome</label>
+                        <input type="text" class="form-control" placeholder="Digite seu nome">
+                    </div>
+
+                    <div class="mb-3">
+                        <label class="form-label">E-mail</label>
+                        <input type="email" class="form-control" placeholder="Digite seu e-mail">
+                    </div>
+
+                    <div class="mb-4">
+                        <label class="form-label">Senha</label>
+                        <input type="password" class="form-control" placeholder="Digite sua senha">
+                    </div>
+
+                    <button class="btn-custom">
+                      <a href="pagina2.php" class="text-light">Entrar</a>
+                    </button>
+
+                    <div class="login-link">
+                        Já possui conta?
+                        <a href="criarconta.php">Criar conta</a>
+                    </div>
+
+                </form>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</body>
+
+</html>
---
+++ pagina2.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="css/main.css">
+
+    <title>Página Principal</title>
+
+</head>
+<body>
+
+    <div class="d-flex">
+
+        <!-- SIDEBAR -->
+        <div class="sidebar d-flex flex-column flex-shrink-0 p-3 text-white">
+
+            <a href="#" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+
+                <i class="bi bi-bootstrap fs-2 me-2"></i>
+
+                <span class="fs-4 fw-bold">
+                    Barra lateral
+                </span>
+
+            </a>
+
+            <hr>
+
+            <ul class="nav nav-pills flex-column mb-auto">
+
+                <li class="nav-item">
+
+                    <a href="#" class="nav-link active">
+
+                        <i class="bi bi-house me-2"></i>
+
+                        Lar
+
+                    </a>
+
+                </li>
+
+                <li>
+
+                    <a href="#" class="nav-link text-white">
+
+                        <i class="bi bi-speedometer2 me-2"></i>
+
+                        Painel
+
+                    </a>
+
+                </li>
+
+                <li>
+
+                    <a href="#" class="nav-link text-white">
+
+                        <i class="bi bi-table me-2"></i>
+
+                        Pedidos
+
+                    </a>
+
+                </li>
+
+                <li>
+
+                    <a href="#" class="nav-link text-white">
+
+                        <i class="bi bi-grid me-2"></i>
+
+                        Produtos
+
+                    </a>
+
+                </li>
+
+                <li>
+
+                    <a href="#" class="nav-link text-white">
+
+                        <i class="bi bi-people me-2"></i>
+
+                        Clientes
+
+                    </a>
+
+                </li>
+
+            </ul>
+
+            <hr>
+
+            <!-- PERFIL -->
+            <div class="dropdown">
+
+                <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
+                    data-bs-toggle="dropdown">
+
+                    <img src="https://github.com/mdo.png" width="32" height="32" class="rounded-circle me-2">
+
+                    <strong>mdo</strong>
+
+                </a>
+
+                <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
+
+                    <li>
+                        <a class="dropdown-item" href="#">
+                            Novo projeto
+                        </a>
+                    </li>
+
+                    <li>
+                        <a class="dropdown-item" href="#">
+                            Configurações
+                        </a>
+                    </li>
+
+                    <li>
+                        <a class="dropdown-item" href="#">
+                            Perfil
+                        </a>
+                    </li>
+
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+
+                    <li>
+                        <a class="dropdown-item" href="#">
+                            Sair
+                        </a>
+                    </li>
+
+                </ul>
+
+            </div>
+
+        </div>
+
+        <!-- CONTEÚDO -->
+        <div class="content">
+
+
+        </div>
+        
+    </div>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
